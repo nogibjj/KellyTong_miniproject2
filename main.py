8501c81f6f54fa85e0e5fa90d774c91c3642254d
Kelly064 <hong22@emory.edu>
2023-09-07 22:33:35 -0400

    change11

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ import pandas as pd
 def create_dataframe():
     data = {'Name': ['Tom', 'Jack', 'nick', 'juli'],
         'marks': [99, 98, 95, 90]}
-    print(data)
+    dataframe = pd.DataFrame(data)
+    dataframe
+
 
 #def desrcibe_dataframe():
-    #data = {'height': [170,175,160,180,190,185,180, 155]
+    #data = {'height': [170,175,160,180,190,185,180,155]
    # }
    # dataframe = pd.DataFrame(data)
    # return dataframe.describe()
