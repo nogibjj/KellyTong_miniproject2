5387ea4cdc5f888b461c71bdb5cbf41d7b7eb11f
Kelly064 <hong22@emory.edu>
2023-09-07 22:08:34 -0400

    change5

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import pandas as pd
 def desrcibe_dataframe():
     x = {'height':[170,175,160,180,190,185,180, 155]}
     dataframe = pd.DataFrame(x)
-    return data.frame.describe()
+    return dataframe.describe()
 
-def absolute_dataframe():
-    x = {'money':[100,200,-100,-200,300,-100000]}
-    dataframe = pd.DataFrame()
-    value = dataframe.abs(x)
-    return value
+#def absolute_dataframe():
+   # x = {'money':[100,200,-100,-200,300,-100000]}
+   # dataframe = pd.DataFrame()
+   # value = dataframe.abs(x)
+    #return value
