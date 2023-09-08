2c4bd691ebc2d374f93caa319222fa6fd187f813
Kelly064 <hong22@emory.edu>
2023-09-07 22:40:57 -0400

    change15

--- main.py
+++ main.py
@@ -1,17 +1,10 @@
 import pandas as pd
 
-def create_dataframe():
-    data = {'Name': ['Tom', 'Jack', 'nick', 'juli'],
-        'marks': [99, 98, 95, 90]}
+def desrcibe_dataframe():
+    data = {'height': [170,175,160,180,190,185]
+    }
     dataframe = pd.DataFrame(data)
-    return print(dataframe)
-
-
-#def desrcibe_dataframe():
-    #data = {'height': [170,175,160,180,190,185,180,155]
-   # }
-   # dataframe = pd.DataFrame(data)
-   # return dataframe.describe()
+    return dataframe.describe()
 
 #def absolute_dataframe():
    # x = {'money':[100,200,-100,-200,300,-100000]}
--- test_main.py
+++ test_main.py
@@ -1,18 +1,15 @@
 #this is for testing functions in main.py
 
-from main import create_dataframe
+from main import desrcibe_dataframe
 
-def test_create():
-    c = create_dataframe()
-    assert c == {'Name': ['Tom', 'Jack', 'nick', 'juli'],
-        'marks': [99, 98, 95, 90]}
-#def test_describe():
+
+def test_describe():
    # "testing the desrcibe_dataframe function in main.py"
-   # describe = desrcibe_dataframe()
-   # assert describe.loc['count'][0]== 8
+    describe = desrcibe_dataframe()
+    assert describe.loc['count'][0]== 6.0
    # assert describe.loc['mean'][0] == 174.375
-   # assert describe.loc['min'] == 155
-   # assert describe.loc['max'] == 190
+   # assert describe.loc['min'] == 155.0
+   # assert describe.loc['max'] == 190.0
 
 #def test_absolute():
   #  "testing the absolute_dataframe function in main.py"
