8995568a94351906f89f466033c7d1469d27cc4f
Kelly064 <hong22@emory.edu>
2023-09-07 22:28:23 -0400

    change9

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import pandas as pd
 
 def desrcibe_dataframe():
-    x = {'height':[170,175,160,180,190,185,180, 155]}
-    dataframe = pd.DataFrame(x)
+    data = {'height': [170,175,160,180,190,185,180, 155]
+    }
+    dataframe = pd.DataFrame(data)
     return dataframe.describe()
 
 #def absolute_dataframe():
