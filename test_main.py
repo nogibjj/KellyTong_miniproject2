0804c82fe8a881f34de95fce8b504add4e4c96a1
Kelly064 <hong22@emory.edu>
2023-09-07 22:49:36 -0400

    absolute

--- test_main.py
+++ test_main.py
@@ -1,6 +1,6 @@
 #this is for testing functions in main.py
 
-from main import desrcibe_dataframe
+from main import desrcibe_dataframe, absolute_dataframe
 
 
 def test_describe():
@@ -11,7 +11,7 @@ def test_describe():
     assert describe.loc['min'] == 155.0
     assert describe.loc['max'] == 190.0
 
-#def test_absolute():
-  #  "testing the absolute_dataframe function in main.py"
-   # absolute = absolute_dataframe()
-   # assert absolute == [100,200,100,200,300,100000]
+def test_absolute():
+    "testing the absolute_dataframe function in main.py"
+    absolute = absolute_dataframe()
+    assert absolute == [100,200,100,200,300,100000]
