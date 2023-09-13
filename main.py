8774103c3f14e1c18cf2ebc0b5f2d3b4813f093a
Kelly0604 <142815940+Kelly0604@users.noreply.github.com>
2023-09-13 15:57:40 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
+def marital():
+    df=pd.read_csv("gss.csv")
+    print(df.shape)
+    print(df.describe())
+    df.plot(x='Age', y='Marital', kind = 'Scatter', title="Correlation between Age and Marital Stage")
+    plt.savefig('Age_Marital.png')
+    
 def desrcibe_dataframe():
     data = {'height': [170,175,160,180,190]
     }
