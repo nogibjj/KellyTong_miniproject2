6b415387cc7ee047b03fddf68c5b16bd1da816f0
Hoi Mei Tong <142815940+Kelly0604@users.noreply.github.com>
2023-09-19 00:08:05 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,6 +1,6 @@
 #this is for testing functions in main.py
 import pandas as pd
-from main import desrcibe_dataframe, describe, age, generate_general_markdown
+from main import describe, age, generate_general_markdown
 
 data = pd.read_csv("gss.csv")
 
@@ -13,10 +13,10 @@ def test_describe():
 def test_md():
    generate_general_markdown(data)
 
-def test_describe():
+#def test_describe():
    # "testing the desrcibe_dataframe function in main.py"
-    describe = desrcibe_dataframe()
-    assert describe.loc['count'][0]== 5.0
-    assert describe.loc['mean'][0] == 175.0
-    assert describe.loc['min'][0] == 160.0
-    assert describe.loc['max'][0] == 190.0
+    #describe = desrcibe_dataframe()
+    #assert describe.loc['count'][0]== 5.0
+    #assert describe.loc['mean'][0] == 175.0
+    #assert describe.loc['min'][0] == 160.0
+    #assert describe.loc['max'][0] == 190.0
