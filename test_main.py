baa3b4aa96c4f69527ae16eaa5eda73065d5beb6
Hoi Mei Tong <142815940+Kelly0604@users.noreply.github.com>
2023-09-18 23:54:06 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,9 +1,17 @@
 #this is for testing functions in main.py
+import panda as pd
+from main import desrcibe_dataframe, describe, age, generate_general_markdown
 
-from main import desrcibe_dataframe, age
+data = pd.read_csv("gss.csv")
 
 def test_age():
-   age()
+   age(data)
+
+def test_describe()
+   describe(data)
+
+def test_md()
+   generate_general_markdown(data)
 
 def test_describe():
    # "testing the desrcibe_dataframe function in main.py"
